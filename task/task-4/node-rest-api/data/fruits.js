3429ff4402640b83d994237289978fb7354d4138
NelanJoe <nelanjorelat@gmail.com>
2022-12-02 20:41:54 +0700

    add new task

---
+++ task/task-4/node-rest-api/data/fruits.js
@@ -0,0 +1,10 @@
+/**
+ * TODO 1:
+ * - Buat array yang berisi data buah.
+ * - Refactor variable ke ES6 Variable.
+ */
+let fruits = ["Apple", "Grape", "Mango", "Banana"];
+
+// TODO 2: export variable fruits
+
+module.exports = fruits;
