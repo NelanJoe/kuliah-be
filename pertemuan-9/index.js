15b1ce425f08de4f614c22ce5657502be6c5ca38
NelanJoe <nelanjorelat@gmail.com>
2022-12-03 15:36:19 +0700

    add: praktikum-9

---
+++ pertemuan-9/index.js
@@ -0,0 +1,30 @@
+/**
+ * * Function
+ *
+ */
+
+// function hitungLuas(jari) {
+//   const PHI = 22 / 7;
+//   const luas = PHI * jari * jari;
+//   return luas;
+// }hitungLuas
+
+// const hitungLuas = function (jari) {
+//   const PHI = 22 / 7;
+//   const luas = PHI * jari * jari;
+//   return luas;
+// };
+
+const hitungLuas = (jari) => 3.14 * jari * jari;
+
+console.log(hitungLuas(10));
+
+/**
+ * * Object
+ * */
+
+const user = {
+  name: "Nelan",
+  age: 22,
+  address: "Karawang, Indonesia",
+};
